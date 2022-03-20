beb9df8c5641ca0e60d92178c9df8cf0d6773876
Asmita Singh <96052563+Asmi1710@users.noreply.github.com>
2022-03-20 23:43:14 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Heading from "./Heading";
+import List from "./List";
+
+//import App from "./App";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Chicken butter masala</li>
+      <li>Chole-chawal</li>
+      <li>Shahi Paneer</li>
+    </ul>
+  );
+}
+
+export default List;
